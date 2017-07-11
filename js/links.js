2988be97e086e92396322969053abe233ce2beff
David Arnedo Gallardo <darnedo@VICTRAN.COM>
2017-07-11 15:04:27 +0200

    Version 1 | Upload all files

---
+++ js/links.js
@@ -0,0 +1,8 @@
+var links = [{
+    "image" :   "desktop_windows",
+    "title" :   "AndTrans - PC",
+    "link"  :   "http://red.victransa.com/WE/STQ_sound/menu.asp?LANID=3",
+},
+{
+
+}];
---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* Url */
+ $('#box-1').click(function(){
+    window.open("https://www.w3schools.com");
+ });
+
+
+
+ function openLink(box) {
+    switch (box){
+        case 1:
+            window.open("http://red.victransa.com/WE/STQ_sound/menu.asp?LANID=3");
+        break;
+        case 2:
+            window.open("http://red.victransa.com/CLT/LOGINFRM.asp");
+        break;
+        case 3:
+            window.open("https://docs.google.com/spreadsheets/d/1uOZg6gSAbalWmzgSMkVgiYKjL-4j5ycEgk8Fe_yox6k/edit?usp=drive_web");
+        break;
+        case 4:
+            window.open("https://docs.google.com/spreadsheets/d/1kBaJ92i1vUXOq3SpQYKJY_1OK-IbzDdhJiRB1BYlwvg/htmlview");
+        break;
+     }
+ }
